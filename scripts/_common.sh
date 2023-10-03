84f1871280d3a67e65391c7664307656c234239b
orhtej2 <2871798+orhtej2@users.noreply.github.com>
2023-10-03 13:27:39 +0200

    List rquired PHP 8.1 deps.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,8 @@
 # COMMON VARIABLES
 #=================================================
 
+pkg_dependencies="php8.1-mbstring php8.1-iconv php8.1-simplexml php8.1-mysqli mariadb-server"
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
