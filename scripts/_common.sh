e4c3befb5338f5dd4ff251c226dca43480198024
Félix Piédallu <felix@piedallu.me>
2023-12-11 22:19:47 +0100

    fix ynh_setup_source workaround

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,14 @@
 # PERSONAL HELPERS
 #=================================================
 
+_check_if_source_available() {
+    source_id=$1
+
+    source_url=$(cat "$YNH_APP_BASEDIR/manifest.toml" | toml_to_json | jq ".resources.sources[\"$source_id\"][\"url\"]")
+
+    curl --output /dev/null --silent --head --fail "$source_url"
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
